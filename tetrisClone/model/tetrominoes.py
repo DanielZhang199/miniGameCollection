f66fe61703cddb28f89c3b066370278ffca1ddf4
Daniel <d.zhang12323@gmail.com>
2023-05-22 02:33:13 -0600

    Implemented all methods for manipulating the pieces, except for rotation. I Piece will now spawn in correct position. Other pieces are WIP (and will raise exceptions).

--- tetrisClone/model/tetrominoes.py
+++ tetrisClone/model/tetrominoes.py
@@ -30,9 +30,10 @@ def num_to_piece(pf: Playfield, n: int):
 
 class Piece:
     def __init__(self, pf: Playfield):
+        self._coordinates = None
         if type(self) is Piece:
             raise Exception('Piece is an abstract class and cannot be instantiated directly')
-        self._corner = [7, 22]  # x, y position of top left corner of 'hit-box' for rotations
+        self._corner = (3, 21)  # x, y position of top left corner of 'hit-box' for rotations
         self._rotation = 0
         self._colour = choice(COLOURS)
         self._field = pf
@@ -44,24 +45,66 @@ class Piece:
     def get_colour(self):
         return self._colour
 
+    def get_coordinates(self):
+        return self._coordinates
+
+    def left(self):
+        """
+        moves piece one to the left if possible, otherwise this function does not do anything
+        :return: nothing
+        """
+        new = []
+        for coord in self._coordinates:
+            if not self._field.is_clear(coord[0] - 1, coord[1]):
+                return
+            new.append((coord[0] - 1, coord[1]))
+        self._coordinates = tuple(new)
+        self._corner = (self._corner[0] - 1, self._corner[1])
+
+    def right(self):
+        """
+        moves piece one to the right if possible
+        :return: nothing
+        """
+        new = []
+        for coord in self._coordinates:
+            if not self._field.is_clear(coord[0] + 1, coord[1]):
+                return
+            new.append((coord[0] + 1, coord[1]))
+        self._coordinates = tuple(new)
+        self._corner = (self._corner[0] + 1, self._corner[1])
+
+    def drop(self):
+        """
+        drops the piece by one, if piece can no longer move down, places it on field
+        :return: (boolean) true if piece was able to move
+        """
+        new = []
+        for coord in self._coordinates:
+            if not self._field.is_clear(coord[0], coord[1] - 1):
+                self._field.add_blocks(self._coordinates, self._colour)
+                return False
+            new.append((coord[0], coord[1] - 1))
+        self._coordinates = tuple(new)
+        self._corner = (self._corner[0], self._corner[1] - 1)
+        return True
+
+    def hard_drop(self):
+        """
+        calls drop over and over until it returns false
+        :return: nothing
+        """
+        while self.drop():
+            pass
+
 
 class IPiece(Piece):
     def __init__(self, pf):
         super().__init__(pf)
-        # list of coordinates of each piece
-        self._coordinates = [[7, 21], [8, 21], [9, 21], [10, 21]]
+        # list of coordinates of each piece when spawning
+        self._coordinates = ((3, 20), (4, 20), (5, 20), (6, 20))
 
-    @staticmethod
-    def get_piece_type():
-        return "IPiece"
-
-    # TODO: following functions
-    def drop(self):
-        """
-        drops the piece by one
-        :return: (boolean) true if piece was able to move
-        """
-        pass
+    # TODO: rotate function for everything
 
     def rotate(self):
         """
@@ -70,28 +113,8 @@ class IPiece(Piece):
         """
         pass
 
-    def hard_drop(self):
-        """
-        calls drop over and over until it returns false
-        :return: nothing
-        """
-        pass
 
-    def left(self):
-        """
-        moves piece one to the left if possible, otherwise this function does not do anything
-        :return: nothing
-        """
-        pass
-
-    def right(self):
-        """
-        moves piece one to the right if possible
-        :return: nothing
-        """
-        pass
-
-
+# TODO: fix coordinates to use tuple and have correct coordinates (x = x - 4, y = y - 1)
 class ZPiece(Piece):
     def __init__(self, pf):
         super().__init__(pf)
@@ -143,10 +166,40 @@ class JPiece(Piece):
 
 
 class OPiece(Piece):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, pf):
+        super().__init__(pf)
         self._coordinates = [[8, 21], [8, 22], [9, 21], [9, 22]]
 
     @staticmethod
     def get_piece_type():
         return "OPiece"
+
+
+if __name__ == "__main__":
+    field = Playfield()
+    piece = IPiece(field)
+    print(piece.get_coordinates(), piece.get_corner_position())
+    piece.drop()
+    print(piece.get_coordinates(), piece.get_corner_position())
+    piece.left()
+    print(piece.get_coordinates(), piece.get_corner_position())
+    piece.left()
+    piece.left()
+    piece.left()
+    print(piece.get_coordinates(), piece.get_corner_position())
+    piece.right()
+    print(piece.get_coordinates(), piece.get_corner_position())
+    piece.right()
+    piece.right()
+    piece.right()
+    piece.right()
+    piece.right()
+    piece.right()
+    piece.right()
+    print(piece.get_coordinates(), piece.get_corner_position())
+
+    piece.hard_drop()
+    for i in range(22):
+        for j in range(10):
+            print(field.is_clear(j, 21 - i), end="")
+        print()
