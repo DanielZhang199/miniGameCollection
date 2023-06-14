fa4c4c7216bb6438dd50aac44e7b8844d0e58b69
Daniel <d.zhang12323@gmail.com>
2023-06-14 03:22:17 -0600

    Changed the import statement to reduce the redundancy (caused by auto refactor of filename).

--- tetrisClone/tetrominoes.py
+++ tetrisClone/tetrominoes.py
@@ -1,4 +1,4 @@
-from tetrisClone.playfield import Playfield
+from playfield import Playfield
 
 DEFAULT_WALL_KICK_DATA = {"0>1": ((-1, 0), (-1, 1), (0, -2), (-1, -2)), "1>0": ((1, 0), (1, -1), (0, 2), (1, 2)),
                           "1>2": ((1, 0), (1, -1), (0, 2), (1, 2)), "2>1": ((-1, 0), (-1, 1), (0, -2), (-1, -2)),
