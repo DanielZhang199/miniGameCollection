fe0111d61dffcce9b7e490cd04c4a407d3a7e8fa
Daniel <d.zhang12323@gmail.com>
2023-07-28 03:41:14 -0600

    Fixed issue with vertical spacing and added coordinate values onto grid.

--- hexChess/window.py
+++ hexChess/window.py
@@ -22,39 +22,43 @@ def create_hexagon_window():
     canvas = tk.Canvas(window, width=1000, height=1000)
     canvas.pack()
 
-    hex_size = 50
+    hex_radius = 50  # radius of 50 (long diagonal of 100)
 
-    num_columns = 11
-    center = num_columns // 2 + 1  # =6
+    columns = "abcdefghijk"
+    center = len(columns) // 2 + 1  # =6
 
     start_x = 130
     start_y = 75
     x = start_x
+    squares_coord_map = {}
 
     last_row_colour = -1
-    for col in range(num_columns):
-        if col < center:
-            num_rows = col + center
+    for i, col in enumerate(columns):
+        if i < center:
+            num_rows = i + center
             last_row_colour += 1
             if last_row_colour > 2:
                 last_row_colour = 0
         else:
-            num_rows = num_columns - col + center - 1
+            num_rows = 2 * center - i + 4
             last_row_colour -= 1
             if last_row_colour < 0:
                 last_row_colour = 2
-        y = start_y + (num_columns - num_rows) * hex_size * 0.866
+        y = start_y + (len(columns) - num_rows) * hex_radius * (13 / 15)  # apothem is 13/15 of the radius
         next_colour = last_row_colour
-        for row in range(num_rows):
-            draw_hexagon(canvas, x, y, hex_size, colours[next_colour])
+        for row in range(num_rows, 0, -1):
+            draw_hexagon(canvas, x, y, hex_radius, colours[next_colour])
+            canvas.create_text(x, y, text=f"{col}{row}", fill="black", font='Helvetica 15 bold')
             next_colour += 1
             if next_colour > 2:
                 next_colour = 0
-            y += hex_size * 1.67
+            y += hex_radius * (26 / 15)   # height of each hex is two times apothem
+            squares_coord_map[f"{col}{row}"] = x, y
 
-        x += hex_size * 1.5
+        x += hex_radius * 1.5
+        # width of hexagon is 2 times radius, but we inset each new column halfway into second radius to form grid
 
-
+    # print(squares_coord_map)
     window.mainloop()
 
 
